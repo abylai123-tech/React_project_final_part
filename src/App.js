24986d506247eb2a735764e42c7998cd3e3ae67b
Abeke <abylai.oryntaymail.ru>
2024-02-18 18:45:42 +0600

    Project React

--- src/App.js
+++ src/App.js
@@ -1,33 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useMemo} from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import './styles/App.css';
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
+import PostForm  from "./components/PostForm";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
+import MySelect from "./components/UI/select/MySelect";
+import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
+import { usePosts } from "./components/hooks/usePosts";
+import axios from "axios";
 
-function App() {
-  const [posts, setPosts ] =  useState([
-      {id:1, title: 'JavaScript ', body: 'Description'},
-      {id:2, title: 'JavaScript 2', body: 'Description'},
-      {id:3, title: 'JavaScript 3', body: 'Description'},
-  ])
-  const [title, setTitle] = useState('asdasd')
+function App() {  
+  const [posts, setPosts ] =  useState([])
+  const [filter, setFilter] = useState({sort: '', query: ''})
+  const [modal, setModal] = useState(false);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
-  const addNewPost = () => {
-    console.log(title)
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
+    setModal(false)
   }
 
-  return (
-    <div className="App">
-      <form>
-          {/* Управляемый компонент*/}
-          <MyInput value={title} onChange={e => setTitle(e.target.value) } type="text" placeholder="Название поста"/>
-          <MyInput type="text" placeholder="Описание поста"/>
-          <MyButton onClick={addNewPost}>Создать пост</MyButton>
-      </form>
-        <PostList posts={posts} title="Посты про JS"/>
+  async function fetchPosts() {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    setPosts(response.data)
+  }
+
+  // Получаем пост из дочерного компонента
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
+
+  return ( 
+    <div className="App">   
+        <MyButton style={{marginTop: '30px'}} onClick={() => setModal(true)}>
+            Создать пользователя
+        </MyButton>
+        <MyModal visible={modal} setVisible={setModal}>
+            <PostForm create={createPost}/>
+        </MyModal>
+        <hr style={{margin: '15px 0'}}/>
+        <PostFilter 
+          filter={filter} 
+          setFilter={setFilter}
+        />
+        <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS"/>
     </div>
   );
 }
